475223a11aada37b54fd78b8cab386e79b300c86
shagun303 <shagunmishra638@gmail.com>
2026-03-27 10:48:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.7 MB (62.69%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.java
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,26 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k=0;
-        // int[] freq = new int [101];
-        // for(int i = 0;i < nums.length; i++){
-        //     int fCount = nums[i];
-        //     freq[fCount] +=1;
-        // }
-        // for(int i = 0;i < nums.length;i++){
-        //     if(freq[i] >=1){
-        //         nums[k++] = nums[i];
-        //         continue;
-              
-        //     }
-          
-        // }
-        int j=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[j-1]){
-                nums[j]=nums[i];
-                j++;
-            }
+        int i = 0;
+       for(int j = 1;j < nums.length;j++){
+        if(nums[j] != nums[i]){
+            i++;
+            nums[i] = nums[j];
         }
-      return j;
+       }
+       return i+1;
     }
 }
