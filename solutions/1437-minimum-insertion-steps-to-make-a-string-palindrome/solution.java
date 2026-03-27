98b19798985186e4ebb42be88f91fb90053dbfde
shagun303 <shagunmishra638@gmail.com>
2026-03-27 06:48:51 +0000

    Sync LeetCode submission Runtime - 27 ms (48.68%), Memory - 47.6 MB (29.99%)

---
+++ solutions/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minInsertions(String s) {
+      int n = s.length();
+    int LCS[][] = new int[n+1][n+1];
+    String t = new StringBuilder(s).reverse().toString();
+
+    for(int i = 1 ;i <= n;i++){
+        for(int j = 1;j <= n;j++){
+            if(s.charAt(i-1) == t.charAt(j-1)){
+                LCS[i][j] = 1 + LCS[i-1][j-1];
+
+            }
+            else{
+                LCS[i][j] = Math.max(LCS[i-1][j],LCS[i][j-1]);
+            }
+        }
+    }return n-LCS[n][n];  
+    }
+}
