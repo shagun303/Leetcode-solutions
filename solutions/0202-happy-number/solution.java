7b91bf22451d5e549fb4f50d943e26345c8ade08
shagun303 <shagunmishra638@gmail.com>
2026-06-17 20:30:07 +0000

    Sync LeetCode submission Runtime - 1 ms (77.44%), Memory - 42.3 MB (66.15%)

---
+++ solutions/0202-happy-number/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while(fast != 1){
+            slow = sumOfn(slow);
+            fast = sumOfn(fast);
+            fast = sumOfn(fast);
+
+            if(slow == fast && slow != 1){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+
+    public int sumOfn(int n){
+        int sum = 0;
+
+        while(n > 0){
+            int d = n % 10;
+            n = n / 10;
+           sum = sum + d * d;
+        }
+
+        return sum;
+    }
+}
